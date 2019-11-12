a2acbe141b90babf8bcbb8abcf25d31f15e41e1d
Brett Karr <b.karr@polyclip.us>
2019-11-12 15:15:03 -0600

    commit with all MVP met, and stretch goals met (remove items AND storing data in localStorage). Works with refreshing the page as well

--- src/App.js
+++ src/App.js
@@ -16,33 +16,36 @@ function App() {
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
+    //sets the cart to whatever is in localStorage upon page refresh
     if (JSON.parse(localStorage.getItem("product-list"))) {
       setCart(JSON.parse(localStorage.getItem("product-list")));
     }
   }, []);
 
+  useEffect(() => {
+    //sets the localStorage to cart values when cart is updated
+    setStorageLocal(cart);
+  }, [cart]);
+
+  //----functions----
   const addItem = item => {
     setCart([...cart, item]);
-    setStorageLocal(cart);
+    // setStorageLocal(cart);
   };
-
   const removeItem = id => {
     const filteredCart = cart.filter(each => each.id !== id);
     setCart(filteredCart);
   };
-
   const setStorageLocal = data => {
-    // localStorage.setItem("product-list", cartInfo ? cartInfo : "");
     localStorage.setItem("product-list", JSON.stringify(data));
   };
-
-  useEffect(() => {}, [cart]);
+  ///----end functions-----
 
   return (
     <ProductContext.Provider value={{ products, addItem }}>
       <CartContext.Provider value={{ cart, removeItem }}>
         <div className="App">
-          {console.log(JSON.parse(localStorage.getItem("product-list")))}
+          {console.log(JSON.parse(localStorage.getItem("product-list")), cart)}
           <Navigation />
           <Route exact path="/" component={Products} />
           <Route path="/cart" component={ShoppingCart} />
