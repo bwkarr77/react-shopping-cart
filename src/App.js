53c41247e5897ea3e4e55dd41a7c4df667c49e86
Brett Karr <b.karr@polyclip.us>
2019-11-11 22:45:11 -0600

    commit with stretch goal reached

--- src/App.js
+++ src/App.js
@@ -20,9 +20,14 @@ function App() {
     console.log(cart);
   };
 
+  const removeItem = id => {
+    const filteredCart = cart.filter(each => each.id !== id);
+    setCart(filteredCart);
+  };
+
   return (
     <ProductContext.Provider value={{ products, addItem }}>
-      <CartContext.Provider value={{ cart }}>
+      <CartContext.Provider value={{ cart, removeItem }}>
         <div className="App">
           {/* <Navigation cart={cart} /> */}
           <Navigation />
